a0a102f15cb9aae703e73bf57b12c41185839323
adowns01 <downs.amelia@gmail.com>
2014-04-20 22:36:35 -0500

    Update gc1_pseudocode.js

--- week_3/5_group_project/gc1_pseudocode.js
+++ week_3/5_group_project/gc1_pseudocode.js
@@ -29,3 +29,25 @@
   // add the elements at positions (array length/2) and (array length/2) +1 together [these are the center to elements]
     // again, make sure to use integer division here
   // divide this sum by 2 and return this answer 
+
+
+
+// Reflection: 
+
+// First, this exercise was an example of why reading instructions is important. My accountability group just saw 
+// that it was a group project and we all assumed that it was a group parining project, when actually, every person
+// worked individually. So we tried to find a time that we could all meet, but failed. Between four busy schedules 
+// and four times zones (Ronald is exactly 12 hours ahead of me!) we just couldn't find a time. 
+// Then I actally read the guidelines and orealized we didn't actually need to carve our a large chunk of time 
+// together. 
+
+// I know that there were some problems with the code with one of the tests and I know that the other group members
+// pulled together and fixed it. I spent the last four days without a computer, so I was unavailable. I feel slightly 
+// guilty about it, but my team mates knew that and I turned in my part early and helped with what I could before I left.
+
+// Doing this project makes me even more excited for working in person because I think that it will be easier to pair 
+// when we are all in the same time zone on relatively the same schedule. 
+
+// I had never heard of the terms "user stories" or "refactoring" before. In the past I have only used pseudo code 
+// before "real" coding. I like breaking it down even more and I can imagine that it would be more helpful with large
+// projects. 
